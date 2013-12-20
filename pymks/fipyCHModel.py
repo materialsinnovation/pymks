e4533c55e852c20e5255a943a3829d6e54d01e7b
Daniel Wheeler <daniel.wheeler2@gmail.com>
2013-12-20 17:43:38 -0500

    Major speed up to `FastMKSRegressionModel` using PyFFTW.
    The `predict` method for the `FastMKSRegressionModel` is now a lot
    faster using PyFFTW as well as a few more Numexpr. Using this class a
    full Cahn-Hilliard simulation using the MKS is possible with a grid
    size of 256x256.
    
     - The `FiPyCHModel` can now be passed the `a` parameter.
    
     - The `FiPyCHModel` does 5 sweeps to ensure that is has converged at
       a given step.
    
     - The `FastMKSModel` takes a `threads` argument so both the threading
       for Numexpr and PyFFTW can be set.
    
     - The entire `predict` method has now been overwritten by
       `FastMKSRegressionMoldel`.
    
    Address #25

--- pymks/fipyCHModel.py
+++ pymks/fipyCHModel.py
@@ -3,13 +3,14 @@ from fipy.solvers.scipy.linearLUSolver import LinearLUSolver
 import numpy as np
 
 class FiPyCHModel(object):
-    def __init__(self, dx=0.25, dy=None, dt=1e-3):
+    def __init__(self, dx=0.25, dy=None, dt=1e-3, a=1.):
         self.dx = dx
         if dy is None:
             self.dy = dx
         else:
             self.dy = dy
         self.dt = dt
+        self.a = a
 
     def fit(self, X, y):
         raise NotImplementedError
@@ -18,18 +19,27 @@ class FiPyCHModel(object):
         S, nx, ny = X.shape
         y = np.zeros(X.shape, dtype='d')
         mesh = fp.PeriodicGrid2D(nx=nx, ny=ny, dx=self.dx, dy=self.dy)
-        phi = fp.CellVariable(mesh=mesh)
+        phi = fp.CellVariable(mesh=mesh, hasOld=True)
         PHI = phi.arithmeticFaceValue
-        D = a = epsilon = 1
+        D = epsilon = 1
         eq = (fp.TransientTerm()
-              == fp.DiffusionTerm(coeff=D * a**2 * (1 - 6 * PHI * (1 - PHI)))
+              == fp.DiffusionTerm(coeff=D * self.a**2 * (1 - 6 * PHI * (1 - PHI)))
               - fp.DiffusionTerm(coeff=(D, epsilon**2)))
 
         for i, x in enumerate(X):
             phi[:] = x.flatten()
-            eq.solve(phi, dt=self.dt, solver=LinearLUSolver())
+            phi.updateOld()
+            self._solve(eq, phi, LinearLUSolver(), self.dt)
+            #            eq.solve(phi, dt=self.dt, solver=LinearLUSolver())
             phi_ij = np.array(phi).reshape((nx, ny))
             y[i] = (phi_ij - x) / self.dt 
 
         return y
+
+    def _solve(self, eq, phi, solver, dt):
+        res = 1e+10
+        for sweep in range(5):
+            res = eq.sweep(phi, dt=dt, solver=solver)
+            #    print 'res',res
+            
 
