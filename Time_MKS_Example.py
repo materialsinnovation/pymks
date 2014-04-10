ac43dbe8589ea539d97e164d5cc3ca6ce3849709
David Brough <david.brough.0416@gmail.com>
2014-04-10 12:34:56 -0400

    Added Example Datasets and change file i/o
    The files DDHO.2.csv,  DDHO.3.csv, and  DDHO.4.csv were added to the
    repo. They are used as an example to calibrate coefficients and predict
    the results. The file I/O were also changed to import the files.

--- Time_MKS_Example.py
+++ Time_MKS_Example.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import TransferFunction as tf
+import os as os
 
 
-fileLocation = 'C:\\Users\\PC\\Desktop\\DDHO.2.csv'
-#calibrationData = pd.io.parsers.read_csv(fileLocation)
-#tf.dataAnalysis(calibrationData)
+fileLocation = os.path.abspath('DDHO.2.csv')
+calibrationData = pd.io.parsers.read_csv(fileLocation)
+tf.dataAnalysis(calibrationData)
 calibrationData = pd.io.parsers.read_csv(fileLocation)
 AR = 4
 D = 0
@@ -17,12 +18,12 @@ model = tf.TFmodel(calibrationData,AR,D,Exo,Delay)
 calibrationData = pd.io.parsers.read_csv(fileLocation)
 
 tf.TFmodelAnalysis(calibrationData,model,AR,D,Exo,Delay)
-fileLoc1 = 'C:\\Users\\PC\\Desktop\\DDHO.3.csv'
-fileLoc2 = 'C:\\Users\\PC\\Desktop\\DDHO.4.csv'
+fileLoc1 = os.path.abspath('DDHO.3.csv')
+fileLoc2 = os.path.abspath('DDHO.4.csv')
 data1 = pd.io.parsers.read_csv(fileLoc1)
 data2 = pd.io.parsers.read_csv(fileLoc2)
 tf.plot_predict(data1,model.params,AR,D,Exo,Delay)
 tf.plot_predict(data2,model.params,AR,D,Exo,Delay)
 data2 = pd.io.parsers.read_csv(fileLoc2)
 predict = tf.yt_predict(data2,model.params,AR,D,Exo,Delay)
-print predict.values[100:150]
+
