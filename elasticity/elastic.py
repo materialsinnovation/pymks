8e6035e634a33c95b1d2d13e66d2fa640d3583f8
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-05-21 11:20:56 -0400

    Internal region with fixed displacement
    Address #49
    
    Example has internal region with fixed displacement. Both the x and y
    boundary conditions are periodic. The example just displaces the
    entire box at the moment.

--- elasticity/elastic.py
+++ elasticity/elastic.py
@@ -27,6 +27,10 @@ def shift_u_fun(ts, coors, bc=None, problem=None, shift=0.0):
     val = shift * nm.ones_like(coors[:, 1])
     return val
 
+def inner_region_(coors, domain=None):
+    x, y = coors[:, 0], coors[:, 1]
+    flag = nm.where((x < 0.2) & (x > -0.2) & (y > -0.2) & (y < 0.2))[0]
+    return flag
 
 usage = """%prog [options]"""
 help = {
@@ -49,8 +53,11 @@ def main():
     min_x, max_x = domain.get_mesh_bounding_box()[:,0]
     eps = 1e-8 * (max_x - min_x)
     min_y, max_y = domain.get_mesh_bounding_box()[:,1]
+
+    inner_region = Function('inner_region',  inner_region_)
     
     omega = domain.create_region('Omega', 'all')
+    omega1 = domain.create_region('Omega1', 'vertices by inner_region', functions=Functions([inner_region]))
     gamma1 = domain.create_region('Gamma1',
                                   'vertices in x < %.10f' % (min_x + eps),
                                   'facet')
@@ -64,6 +71,8 @@ def main():
                                      'vertices in y > %.10f' % (max_y - eps),
                                      'facet')
 
+
+    
     field = Field.from_args('fu', nm.float64, 'vector', omega, approx_order=2)
 
     u = FieldVariable('u', 'unknown', field)
@@ -75,6 +84,7 @@ def main():
         else:
             value = 1. * (coors[:, 0] > .25) + 1.
             value.shape = (coors.shape[0], 1, 1)
+            
             one = nm.ones_like(value)
             return {'lam' : value, 'mu' : one}
 
@@ -91,14 +101,22 @@ def main():
     eq = Equation('balance', t1 + t2)
     eqs = Equations([eq])
 
-    fix_u = EssentialBC('fix_u', gamma1, {'u.all' : 0.0})
+    #fix_u = EssentialBC('fix_u', gamma1, {'u.0' : 0.0})
 
-    bc_fun = Function('shift_u_fun', shift_u_fun, extra_args={'shift' : 0.01})
-    shift_u = EssentialBC('shift_u', gamma2, {'u.0' : bc_fun})
+    # bc_fun = Function('shift_u_fun', shift_u_fun, extra_args={'shift' : 0.0})
+    # shift_u = EssentialBC('shift_u', gamma2, {'u.0' : bc_fun})
 
+    # bc_fun = Function('shift_u_fun', shift_u_fun, extra_args={'shift' : 0.0})
+    # shift_u = EssentialBC('shift_u', gamma2, {'u.0' : bc_fun})
+    
     match_x_line = Function('match_x_line', per.match_x_line)
     periodic_y = PeriodicBC('periodic_y', [gamma_top, gamma_bottom], {'u.all' : 'u.all'}, match='match_x_line')
 
+    match_y_line = Function('match_y_line', per.match_y_line)
+    periodic_x = PeriodicBC('periodic_x', [gamma1, gamma2], {'u.all' : 'u.all'}, match='match_y_line')
+
+    inner_displacement = EssentialBC('inner_displacement', omega1, {'u.0' : 0.01, 'u.1' : 0.0})
+    
     ls = ScipyDirect({})
 
     nls_status = IndexedStruct()
@@ -106,9 +124,13 @@ def main():
 
     pb = Problem('elasticity', equations=eqs, nls=nls, ls=ls)
     pb.save_regions_as_groups('regions')
-    functions = Functions([bc_fun, match_x_line])
-    pb.time_update(ebcs=Conditions([fix_u, shift_u]),
-                   epbcs=Conditions([periodic_y]),
+    # functions = Functions([match_x_line, match_y_line])
+    # pb.time_update(ebcs=Conditions([fix_u]),
+    #                epbcs=Conditions([periodic_y, periodic_x]),
+    #                functions=functions)
+    functions = Functions([match_x_line, match_y_line, inner_region])
+    pb.time_update(ebcs=Conditions([inner_displacement]),
+                   epbcs=Conditions([periodic_y, periodic_x]),
                    functions=functions)
 
     vec = pb.solve()
