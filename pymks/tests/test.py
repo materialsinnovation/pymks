4e5ce488d50f148c4fcca81d8b2ac93d418e307b
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-05-28 18:03:46 -0400

    Test case for MKS elasticity
    Address #49
    
    Add simple test case for MKS elasticity with a delta microstructure.

--- pymks/tests/test.py
+++ pymks/tests/test.py
@@ -1,8 +1,8 @@
-import os
-
-
+from pymks import MKSRegressionModel
 from pymks import ElasticFEModel
 import numpy as np
+from sklearn import metrics
+mse = metrics.mean_squared_error
 
 def test_ElasticFEModel():
     X = np.zeros((1, 5, 5, 2))
@@ -14,5 +14,29 @@ def test_ElasticFEModel():
     y = model.predict(X)
     assert np.allclose(y[0, 2, 2, :], (4.1987e-1, 6.5947e-2, 0), rtol=1e-4)
 
+def test_MKSelastic():
+    elastic_modulus = np.array((1., 1.5))
+    poissons_ratio = np.array((0.3, 0.3))
+
+    nx = 11
+    Lx = 1.
+    dx = Lx / nx
+    ii = (nx - 1) / 2
+
+    X = np.zeros((2, nx, nx), dtype=int)
+    X[0, ii, ii] = 1
+    X[1] = 1 - X[0]
+
+    X_prop = np.concatenate((elastic_modulus[X][...,None], poissons_ratio[X][...,None]), axis=-1)
+
+    elastic_model = ElasticFEModel(dx=dx)
+    y_prop = elastic_model.predict(X_prop)
+
+    for y_test in np.rollaxis(y_prop, -1):
+        model = MKSRegressionModel(Nbin=2)
+        model.fit(X, y_test)
+        y_pred = model.predict(X)
+        assert np.allclose(y_pred, y_test, rtol=1e-1, atol=1e-2)
+    
 if __name__ == '__main__':
-    test_ElasticFEModel()
+    test_MKSelastic()
