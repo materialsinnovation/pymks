1318752cce5c946aacc0e5b2a9ff858fe921eecb
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-05-30 12:32:12 -0400

    Properties attached to quadrature points
    Address #49
    
    Material properties are now attached to quadrature points. Allows
    removal of nasty interpolation of properties from elements to
    vertices. This scheme fits better with the MKS and now allows the MKS
    to accurately predict strains.
    
    There is still an issue with the FFT in the x-direction due to lack of
    periodicity in FE model.

--- pymks/elasticFEModel.py
+++ pymks/elasticFEModel.py
@@ -72,42 +72,10 @@ class ElasticFEModel(object):
         K = E / 3 / (1 - 2 * nu)
         mu = K - lame
         return np.concatenate((lame[...,None], mu[...,None]), axis=-1)
-
-    def cell_to_node(self, X):
-        """
-        Maps the cell values given by X to node values. The values are
-        not interpolated, but mapped out from the center cell.
-
-        Args:
-          X: cell values with shape (Nsample, Nx, Ny, Nproperty)
-
-        Returns:
-          The nodal values with shape (Nsample, Nx + 1, Ny + 1, Nproperty)
-          
-        >>> X = np.arange(15).reshape((1, 5, 3))
-        >>> X_ = np.array([[[  0.,  1.,  1.,  2.],
-        ...                 [  3,   4,   4,   5.],
-        ...                 [  6,   7,   7,   8.],
-        ...                 [  6,   7,   7,   8.],
-        ...                 [  9,  10,  10,  11.],
-        ...                 [ 12,  13,  13,  14.]]])
-        >>> model = ElasticFEModel(1)
-        >>> assert np.all(np.equal(X_, model.cell_to_node(X)))
-        
-        """
-        Nsample, Nx, Ny = X.shape[:3]
-        X_ = np.zeros((Nsample, Nx + 1, Ny + 1) + X.shape[3:])
-        Mx = (Nx + 1) / 2
-        My = (Ny + 1) / 2
-        X_[:, Mx:, My:] = X[:, Mx - 1:, My - 1:]
-        X_[:, :Mx, My:] = X[:, :Mx, My - 1:]
-        X_[:, Mx:, :My] = X[:, Mx - 1:, :My]
-        X_[:, :Mx, :My] = X[:, :Mx, :My]
-        return X_
             
     def predict(self, X):
         """
-        Predict the displacement field give an initial microstructure
+        Predict the displacement field given an initial microstructure
         and a strain in the x direction.
 
         Args:
@@ -120,50 +88,15 @@ class ElasticFEModel(object):
           
         """
         Nsample, Nx, Ny, Nproperty = X.shape
-        if (Nx % 2 == 0) or (Ny % 2 == 0) or (Nproperty != 2):
+        if (Nproperty != 2):
             raise RuntimeError, 'the shape of X is incorrect'
         
-        Xnode = self.cell_to_node(X)
-        
-        X_ = self.convert_properties(Xnode)
+        X_ = self.convert_properties(X)
 
-        y_disp = np.array([self.solve(x) for x in X_])
+        y_strain = np.array([self.solve(x) for x in X_])
 
-        y_strain = self.get_strain(y_disp)
-        
         return y_strain
 
-    def get_strain(self, y_disp):
-        """
-        Calculate the strain field from the displacement field.
-
-        Args:
-        y_disp: the displacement field with shape (Nsample, Nx, Ny, 2)
-                where the last index represents the x and y
-                displacement
-
-        Returns:
-          y_strain: the strain field with shape (Nsample, Nx, Ny, 3)
-          where the last index represents the e_xx, e_yy, e_xy strain
-          fields, respectively
-
-        """
-        
-        xx = (y_disp[:, 1:, :, 0] - y_disp[:, :-1, :, 0]) / self.dx
-        exx = (xx[:, :, 1:] + xx[:, :, :-1]) / 2
-        
-        yy = (y_disp[:, :, 1:, 1] - y_disp[:, :, :-1, 1]) / self.dx
-        eyy = (yy[:, 1:] + yy[:, :-1]) / 2
-
-        xy_ = (y_disp[:, 1:, :, 1] - y_disp[:, :-1, :, 1]) / self.dx
-        xy = (xy_[:, :, 1:] + xy_[:, :, :-1]) / 2.
-        yx_ = (y_disp[:, :, 1:, 0] - y_disp[:, :, :-1, 0]) / self.dx
-        yx = (yx_[:, 1:] + yx_[:, :-1]) / 2
-        exy = (xy + yx) / 2
-
-        return np.concatenate((exx[..., None], eyy[..., None], exy[..., None]), axis=-1)
-        
-    
     def get_material(self, property_array, domain):
         """
         Creates an SfePy material from the material property fields
@@ -179,21 +112,20 @@ class ElasticFEModel(object):
         miny, maxy = domain.get_mesh_bounding_box()[:, 1]
 
         def material_func_(ts, coors, mode=None, **kwargs):
-            if mode != 'qp':
-                return
-            else:
+            if mode == 'qp':
                 x, y = coors[:, 0], coors[:, 1]
                 i_out = np.empty_like(x, dtype=np.int64)
                 j_out = np.empty_like(y, dtype=np.int64)
-                i = np.floor((x + self.dx / 2 - minx) / self.dx, i_out)
-                j = np.floor((y + self.dx / 2 - miny) / self.dx, j_out)
-                
+                i = np.floor((x - minx) / self.dx, i_out)
+                j = np.floor((y - miny) / self.dx, j_out)
                 property_array_ = property_array[i, j]
                 lam = property_array_[..., 0]
                 mu = property_array_[..., 1]
                 lam = np.ascontiguousarray(lam.reshape((lam.shape[0], 1, 1)))
                 mu = np.ascontiguousarray(mu.reshape((mu.shape[0], 1, 1)))
                 return {'lam' : lam, 'mu' : mu}
+            else:
+                return
 
         material_func = Function('material_func', material_func_)
         return Material('m', function=material_func)
@@ -306,10 +238,11 @@ class ElasticFEModel(object):
           Sfepy mesh
           
         """
-        Lx = (shape[0] - 1) * self.dx
-        Ly = (shape[1] - 1) * self.dx
+        Lx = shape[0] * self.dx
+        Ly = shape[1] * self.dx
         center = (0., 0.)
-        return gen_block_mesh((Lx, Ly), shape, center, verbose=False)
+        vertex_shape = (shape[0] + 1, shape[1] + 1)
+        return gen_block_mesh((Lx, Ly), vertex_shape, center, verbose=False)
     
     def solve(self, property_array):
         """
@@ -362,9 +295,12 @@ class ElasticFEModel(object):
                        epbcs=epbcs,
                        functions=functions)
 
-        vec = pb.solve()
+        pb.solve()
 
-        return vec.create_output_dict()['u'].data.reshape(shape + (2,))
+        strain = np.squeeze(pb.evaluate('ev_cauchy_strain.2.region_all(u)', mode='el_avg'))
+
+        return np.reshape(strain, (shape + strain.shape[-1:]))
+
 
 
     
