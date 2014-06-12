d75bb60cb453ad97d22195387e70b1d7c3220d87
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-06-12 15:38:46 -0400

    Add HTML titles, logos and icons.
    Address #52.
    
    The colors for the logo are #58b91a and #f1d71d

--- doc/conf.py
+++ doc/conf.py
@@ -105,19 +105,19 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "PyMKS"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "PyMKS"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'pymks_logo.resize.png'
 
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#html_favicon = None
+# # The name of an image file (within the static path) to use as favicon of the
+# # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# # pixels large.
+html_favicon = 'pymks_logo.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
