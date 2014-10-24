c702dc78eafb3dbd09b3b677851cd829d9d7df56
David Brough <david.brough.0416@gmail.com>
2014-10-24 14:52:44 -0400

    Add test cases for 2-point statistics
    Add four test cases for nonperiodic and periodic autocorrelation and
    crosscorrelation. Change stats to handle a list or tuple for periodic
    axes.

--- pymks/stats.py
+++ pymks/stats.py
@@ -11,9 +11,26 @@ microstructures and the DiscreteIndicatorBasis.
 def autocorrelate(X_, periodic_axes=[]):
     """
     Computes the autocorrelation for a microstructure
+
+    Test non-periodic autocorrelation.
+
+    >>> n_states = 2
+    >>> X = np.array([[[0, 0, 0],
+    ...                [0, 1, 0],
+    ...                [0, 0, 0]]])
+    >>> from pymks.bases import DiscreteIndicatorBasis
+    >>> basis = DiscreteIndicatorBasis(n_states=n_states)
+    >>> X_ = basis.discretize(X)
+    >>> X_auto = autocorrelate(X_)
+    >>> X_test = np.array([[[0., 0., 0., 0., 0.],
+    ...                   [0., 0., 0., 0., 0.],
+    ...                   [0., 0., 1./9, 0., 0.],
+    ...                   [0., 0., 0., 0., 0.],
+    ...                   [0., 0., 0., 0., 0.]]])
+    >>> assert(np.allclose(np.real_if_close(X_auto[0, ..., 1]), X_test[0]))
     """
     s = Fkernel_shape(X_, periodic_axes)
-    return Correlation(X_).convolve(X_) / normalize(X_, s)
+    return Correlation(X_, Fkernel_shape=s).convolve(X_) / normalize(X_, s)
 
 
 def crosscorrelate(X_, periodic_axes=[]):
@@ -61,36 +78,38 @@ def crosscorrelate(X_, periodic_axes=[]):
     Returns:
       Crosscorelations for microstructure X.
     '''
-    
+
     n_states = X_.shape[-1]
     Niter = n_states // 2
     Nslice = n_states * (n_states - 1) / 2
 
     s = Fkernel_shape(X_, periodic_axes)
-    tmp = [Correlation(X_, Fkernel_shape=s).convolve(np.roll(X_, i,
-                                                             axis=-1)) for i in range(1,
-                                                                                      Niter + 1)]
+    tmp = [Correlation(X_,
+                       Fkernel_shape=s).convolve(np.roll(X_, i,
+                                                         axis=-1)) for i
+           in range(1, Niter + 1)]
     return np.concatenate(tmp, axis=-1)[..., :Nslice] / normalize(X_, s)
+
 
 def normalize(X_, Fkernel_shape):
     """
     Returns the normalization for the statistics
 
     The normalization should be Nx * Ny in the center of the domain.
-    
+
     >>> Nx = Ny = 5
     >>> X_ = np.zeros((1, Nx, Ny, 1))
     >>> Fkernel_shape = np.array((2 * Nx, Ny))
     >>> norm =  normalize(X_, Fkernel_shape)
     >>> assert norm.shape == (1, 2 * Nx, Ny, 1)
     >>> assert np.allclose(norm[0, Nx, Ny / 2, 0], 25)
-    
+
     Args:
       X_: discretized microstructure (array)
       Fkernel_shape: the shape of the kernel is Fourier space (array)
     Returns:
-      Normalization 
-      
+      Normalization
+
     """
     if (Fkernel_shape == X_.shape[1:-1]).all():
         return np.prod(X_.shape[1:-1])
@@ -99,6 +118,7 @@ def normalize(X_, Fkernel_shape):
         corr = Correlation(ones, Fkernel_shape=Fkernel_shape)
         return corr.convolve(ones)
 
+
 def Fkernel_shape(X_, periodic_axes):
     """
     Returns the shape of the kernel in Fourier space with non-periodic padding.
@@ -106,13 +126,17 @@ def Fkernel_shape(X_, periodic_axes):
     >>> Nx = Ny = 5
     >>> X_ = np.zeros((1, Nx, Ny, 1))
     >>> periodic_axes = [1]
-    >>> print Fkernel_shape(X_, periodic_axes=periodic_axes)
-    [8 5]
-    
+    >>> assert(Fkernel_shape(X_, periodic_axes=periodic_axes), [8, 5])
+
+    Args:
+      X_ : microstructure funciton
+      periodic_axes: the axes of the array that are periodic
+
+    Returns:
+      Shape of the new Fkernel array
+
     """
     axes = np.arange(len(X_.shape) - 2) + 1
-    a = np.ones(len(axes), dtype=float) * 1.6
-    a[periodic_axes] = 1
+    a = np.ones(len(axes), dtype=float) * 1.98
+    a[list(periodic_axes)] = 1
     return (np.array(X_.shape)[axes] * a).astype(int)
-
-
