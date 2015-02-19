Correct statistics calculation and tests
Address issue #148

Correct statistics calculation to only compute statistics for data
weighted by the confidence interval shown in the uncertainty mask.
The corresponding tests were also updated.

The current version of the code is returning the wrong number of
cross correlations for a 2 local state system. This needs to be
fixed still.

pymks/stats.py
@@ -36,6 +36,7 @@ def autocorrelate(X_, periodic_axes=[], uncertainty_mask=None):
     """
     if uncertainty_mask is not None:
         uncertainty_mask = _mask_check(X_, uncertainty_mask)
+        X_ = X_ * uncertainty_mask
     s = Fkernel_shape(X_, periodic_axes)
     corr = Correlation(X_, Fkernel_shape=s).convolve(X_)
     return truncate(corr, X_.shape[:-1]) / normalize(X_, s, uncertainty_mask)
@@ -91,6 +92,7 @@ def crosscorrelate(X_, periodic_axes=[], uncertainty_mask=None):
 
     if uncertainty_mask is not None:
         uncertainty_mask = _mask_check(X_, uncertainty_mask)
+        X_ = X_ * uncertainty_mask
     n_states = X_.shape[-1]
     Niter = n_states // 2
     Nslice = n_states * (n_states - 1) / 2
@@ -100,7 +102,8 @@ def crosscorrelate(X_, periodic_axes=[], uncertainty_mask=None):
                                                          axis=-1)) for i
            in range(1, Niter + 1)]
     corr = np.concatenate(tmp, axis=-1)[..., :Nslice]
-    return truncate(corr, X_.shape[:-1]) / normalize(X_, s, uncertainty_mask)
+    return truncate(corr, X_.shape[:-1]) /\
+        normalize(X_, s, uncertainty_mask)
 
 
 def correlate(X_, periodic_axes=[], uncertainty_mask=None):

pymks/tests/test_stats.py
@@ -167,9 +167,9 @@ def test_periodic_mask():
     mask[0, 0, 0] = 0
     X_auto_periodic_mask = autocorrelate(X_, periodic_axes=[0, 1],
                                          uncertainty_mask=mask)
-    X_result_0 = np.array([[[3 / 7., 2 / 7., 3 / 7.],
-                          [2 / 7., 5 / 8., 2 / 7.],
-                          [3 / 7., 2 / 7., 3 / 7.]]])
+    X_result_0 = np.array([[[1 / 7., 1 / 7., 3 / 7.],
+                          [1 / 7., 0.5, 1 / 7.],
+                          [3 / 7., 1 / 7., 1 / 7.]]])
     X_result_1 = np.array([[[2 / 7., 1 / 7., 2 / 7.],
                           [1 / 7., 0.5, 1 / 7.],
                           [2 / 7., 1 / 7., 2 / 7.]]])
@@ -192,9 +192,9 @@ def test_nonperiodic_mask():
     mask = np.ones((X.shape))
     mask[0, 0, 0] = 0
     X_auto_nonperiodic_mask = autocorrelate(X_, uncertainty_mask=mask)
-    X_result_0 = np.array([[[2 / 3., 0, 0.5],
-                          [0, 5 / 8., 0.],
-                          [0.5, 0, 2 / 3.]]])
+    X_result_0 = np.array([[[1 / 3., 0, 0.5],
+                          [0, 0.5, 0.],
+                          [0.5, 0, 1 / 3.]]])
     X_result_1 = np.array([[[2 / 3., 0, 0.5],
                           [0, 0.5, 0.],
                           [0.5, 0, 2 / 3.]]])
@@ -215,9 +215,9 @@ def test_mixperdic_mask():
     mask[0, 0, 0] = 0
     X_auto_mixperiodic_mask = autocorrelate(X_, periodic_axes=[0],
                                             uncertainty_mask=mask)
-    X_result_0 = np.array([[[2 / 5., 2 / 7., 2 / 5.],
-                          [0, 5 / 8., 0],
-                          [2 / 5., 2 / 7., 2 / 5.]]])
+    X_result_0 = np.array([[[1 / 5., 1 / 7., 2 / 5.],
+                          [0, 0.5, 0],
+                          [2 / 5., 1 / 7., 1 / 5.]]])
     X_result_1 = np.array([[[2 / 5., 1 / 7., 2 / 5.],
                           [0, 0.5, 0.],
                           [2 / 5., 1 / 7., 2 / 5.]]])