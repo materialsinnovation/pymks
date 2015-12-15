99de01c03fb063ea62c20d2e6c66739021114c1e
Perry W Ellis <perrywmellis@users.noreply.github.com>
2015-12-14 19:43:16 -0500

    moved doctest from ind file to relevent class
    addresses #233
    
    necessary so that not having sfepy will skip the test
    
    doctest is now included in a class that will not be called
    if sfepy is not installed
    
    no side effects

--- pymks/datasets/elastic_FE_simulation.py
+++ pymks/datasets/elastic_FE_simulation.py
@@ -86,6 +86,27 @@ class ElasticFESimulation(object):
 
     >>> assert np.allclose(u[:,0], u[:,-1])
 
+    Check that microstructures outside of the expected range throw
+    the appropriate error
+
+    >>> import numpy as np
+    >>> L = 5
+    >>> elastic_modulus = (1, 2, 3)
+    >>> poissons_ratio = (0.3, 0.3, 0.3)
+    >>> size = (1, L, L)
+    >>> sim = ElasticFESimulation(elastic_modulus=elastic_modulus,
+    ...                           poissons_ratio=poissons_ratio)
+    >>> X = np.zeros(size, dtype=int)
+    >>> sim.run(X)
+    >>> X = np.ones(size, dtype=int)
+    >>> sim.run(X)
+    >>> X[0, 0, 0] = -1
+    >>> sim.run(X)
+    Traceback (most recent call last):
+    ...
+    RuntimeError: X must be between 0 and 2.
+    
+
     """
 
     def __init__(self, elastic_modulus, poissons_ratio, macro_strain=1.,):
