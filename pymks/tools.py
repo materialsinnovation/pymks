66f5a01f94a0f11fc85a1deff74f86bdb1309c5d
aiskakov <alm2006@gmail.com>
2015-12-14 20:07:26 -0500

    Remove swapaxes
    address #253
    
    Remove all swapaxes from the tools.py so that the imported
    images and 2 point statistics plots are all the same
    orientation.

--- pymks/tools.py
+++ pymks/tools.py
@@ -169,12 +169,12 @@ def draw_microstructure_strain(microstructure, strain):
     cmap = _get_response_cmap()
     fig = plt.figure(figsize=(8, 4))
     ax0 = plt.subplot(1, 2, 1)
-    ax0.imshow(microstructure.swapaxes(0, 1), cmap=_get_microstructure_cmap(),
+    ax0.imshow(microstructure, cmap=_get_microstructure_cmap(),
                interpolation='none')
     ax0.set_xticks(())
     ax0.set_yticks(())
     ax1 = plt.subplot(1, 2, 2)
-    im1 = ax1.imshow(strain.swapaxes(0, 1), cmap=cmap, interpolation='none')
+    im1 = ax1.imshow(strain, cmap=cmap, interpolation='none')
     ax1.set_xticks(())
     ax1.set_yticks(())
     ax1.set_title(r'$\mathbf{\varepsilon_{xx}}$', fontsize=25)
@@ -294,14 +294,14 @@ def _draw_fields(fields, field_cmap, fontsize, titles):
     fig, axs = plt.subplots(1, n_fields, figsize=(n_fields * 4, 4))
     if n_fields > 1:
         for field, ax, title in zip(fields, axs.flat, titles):
-            im = ax.imshow(field.swapaxes(0, 1),
+            im = ax.imshow(field,
                            cmap=field_cmap, interpolation='none',
                            vmin=vmin, vmax=vmax)
             ax.set_xticks(())
             ax.set_yticks(())
             ax.set_title(title, fontsize=fontsize)
     else:
-        im = axs.imshow(fields[0].swapaxes(0, 1), cmap=field_cmap,
+        im = axs.imshow(fields[0], cmap=field_cmap,
                         interpolation='none', vmin=vmin, vmax=vmax)
         axs.set_xticks(())
         axs.set_yticks(())
@@ -410,7 +410,7 @@ def draw_gridscores_matrix(grid_scores, params, score_label=None,
         ax.set_xlabel(x_label, fontsize=14)
         ax.set_ylabel(y_label, fontsize=14)
         ax.grid(False)
-        im = ax.imshow(np.swapaxes(matrix, 0, 1),
+        im = ax.imshow(matrix,
                        cmap=X_cmap, interpolation='none')
         ax.set_title(title, fontsize=22)
         divider = make_axes_locatable(ax)
