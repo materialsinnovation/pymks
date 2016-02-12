9f597ecc6f2cdd96c1570e4b39c84f2ecbaf57e4
David Brough <david.brough.0416@gmail.com>
2016-02-12 12:24:40 -0500

    Remove module class attributes
    address #277
    
    Remove module class attributes. This change is allows for the gsh basis
    to be used with sklearn tools such as GridSearchCV. A deepcopy() of a
    model that uses the basis cannot be made if the class attribute is a
    model.

--- pymks/bases/gsh.py
+++ pymks/bases/gsh.py
@@ -65,16 +65,15 @@ class GSHBasis(_ImagFFTBasis):
         if isinstance(n_states, int):
             n_states = np.arange(n_states)
         if domain is None or domain == 'triclinic':
-            self._symmetry = gsh_tri
-            domain = 'triclinic'
+            self.domain = 'triclinic'
         elif domain == 'hexagonal':
-            self._symmetry = gsh_hex
+            self.domain = 'hexagonal'
         elif domain == 'cubic':
-            self._symmetry = gsh_cub
+            self.domain = 'cubic'
         else:
             raise RuntimeError("invalid crystal symmetry")
-        full_indx = self._symmetry.gsh_basis_info()
-        super(GSHBasis, self).__init__(n_states=n_states, domain=domain)
+        full_indx = self._gsh_basis_info()
+        super(GSHBasis, self).__init__(n_states=n_states, domain=self.domain)
         self.basis_indices = full_indx[self.n_states, :]
 
     def check(self, X):
@@ -86,7 +85,7 @@ class GSHBasis(_ImagFFTBasis):
             raise RuntimeError('X must have 3 angles (in radians) in the ' +
                                'last dimention')
 
-    def _shape_check(self, X, y):
+    def _check_shape(self, X_shape, y_shape):
         """
         Checks the shape of the microstructure and response data to
         ensure that they are correct.
@@ -102,12 +101,12 @@ class GSHBasis(_ImagFFTBasis):
         This is because we assume that Bunge Euler angles are assigned for
         each location in the microstructure
         """
-        if not len(y.shape) > 1:
+        if not len(y_shape) > 1:
             raise RuntimeError("The shape of y is incorrect.")
-        if X.shape[-1] != 3:
+        if X_shape[-1] != 3:
             raise RuntimeError("X must have 3 continuous local states " +
                                "(euler angles in radians) in the last axis.")
-        if y.shape != X.shape[:-1]:
+        if y_shape != X_shape[:-1]:
             raise RuntimeError("The X and y must have the same number of " +
                                "samples and microstructure shape.")
 
@@ -116,7 +115,10 @@ class GSHBasis(_ImagFFTBasis):
         Function to describe the expected output shape of a given
         microstructure X.
         """
-        return X.shape[:-1]  # X has Euler angles, while output is scalar
+        _shape = X.shape[:-1]  # X has Euler angles, while output is scalar
+        if len(_shape) < 2:
+            _shape = (X.shape[0],) + (X.shape[1] / 3,)
+        return _shape
 
     def discretize(self, X):
         """
@@ -145,7 +147,7 @@ class GSHBasis(_ImagFFTBasis):
         """
         self.check(X)
         self._select_axes(X[..., 0])
-        return self._symmetry.gsh_eval(X, self.n_states)
+        return self._gsh_eval(X)
 
     def _reshape_feature(self, X, size):
         """
@@ -161,4 +163,37 @@ class GSHBasis(_ImagFFTBasis):
         Returns:
             microstructure with shape (n_samples, size)
         """
-        return X.reshape((X.shape[0],) + size + (X.shape[-1],))
+        _shape = (X.shape[0],) + size + (3,)
+        return X.reshape(_shape)
+
+    def _gsh_basis_info(self):
+        """
+        Returns a an array with the indices used in the GSH functions for a
+        a specific crystal symmetry.
+        """
+        if self.domain == 'triclinic':
+            return gsh_tri.gsh_basis_info()
+        elif self.domain == 'hexagonal':
+            return gsh_hex.gsh_basis_info()
+        elif self.domain == 'cubic':
+            return gsh_cub.gsh_basis_info()
+
+    def _gsh_eval(self, X):
+        """
+        Discretize `X` based on crystal symmetry.
+
+        Args:
+            X (ND array): The microstructure, an `(n_samples, n_x, ..., 3)`
+                shaped array where `n_samples` is the number of samples,
+                `n_x` is the spatial discretization and the last dimension
+                contains the Bunge Euler angles in radians.
+        Returns:
+            Float valued field of of Generalized Spherical Harmonics
+            coefficients.
+        """
+        if self.domain == 'triclinic':
+            return gsh_tri.gsh_eval(X, self.n_states)
+        elif self.domain == 'hexagonal':
+            return gsh_hex.gsh_eval(X, self.n_states)
+        elif self.domain == 'cubic':
+            return gsh_cub.gsh_eval(X, self.n_states)
