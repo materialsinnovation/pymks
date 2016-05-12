90d6924f91c7407517614e5cd27d92f32364bdac
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-05-12 17:50:35 -0400

    Add travis.yml for Travis CI
    Address #101

--- pymks/tests/test_microstructure_generator.py
+++ pymks/tests/test_microstructure_generator.py
@@ -1,28 +1,28 @@
-from nose.tools import raises
+import pytest
 import numpy as np
 from pymks.datasets import make_microstructure
 
 
-@raises(RuntimeError)
 def test_size_and_grain_size_failure():
-    make_microstructure(n_samples=1, size=(7, 7), grain_size=(8, 1))
+    with pytest.raises(RuntimeError):
+        make_microstructure(n_samples=1, size=(7, 7), grain_size=(8, 1))
 
 
-@raises(RuntimeError)
 def test_volume_fraction_failure():
-    make_microstructure(n_samples=1, volume_fraction=(0.3, 0.6))
+    with pytest.raises(RuntimeError):
+        make_microstructure(n_samples=1, volume_fraction=(0.3, 0.6))
 
 
-@raises(RuntimeError)
 def test_volume_fraction_with_n_phases_failure():
-    make_microstructure(n_samples=1, size=(7, 7), n_phases=3,
-                        volume_fraction=(0.5, 0.5))
+    with pytest.raises(RuntimeError):
+        make_microstructure(n_samples=1, size=(7, 7), n_phases=3,
+                            volume_fraction=(0.5, 0.5))
 
 
-@raises(RuntimeError)
 def test_percent_variance_exceeds_limit_failure():
-    make_microstructure(n_samples=1, size=(7, 7), n_phases=3,
-                        volume_fraction=(0.3, 0.3, 0.4), percent_variance=0.5)
+    with pytest.raises(RuntimeError):
+        make_microstructure(n_samples=1, size=(7, 7), n_phases=3,
+                            volume_fraction=(0.3, 0.3, 0.4), percent_variance=0.5)
 
 
 def test_volume_fraction():
