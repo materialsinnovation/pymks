70146ef33e4daf46b57d13b8cab62eb388be5191
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-09-30 16:37:48 -0400

    Update version number generation

--- pymks/__init__.py
+++ pymks/__init__.py
@@ -19,11 +19,16 @@ def test():
 
 
 def get_version():
+    """Get the version of the code from egg_info.
+
+    Returns:
+      the package version number
+    """
     from pkg_resources import get_distribution, DistributionNotFound
 
     try:
-        version = get_distribution(__name__).version
-    except DistributionNotFound:
+        version = get_distribution(__name__.split('.')[0]).version # pylint: disable=no-member
+    except DistributionNotFound: # pragma: no cover
         version = "unknown, try running `python setup.py egg_info`"
 
     return version
