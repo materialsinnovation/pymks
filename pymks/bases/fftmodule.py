eaefc567af6b0065b30e4c0967d56685ed7fe5d0
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-11-02 10:56:40 -0400

    Make the tests use the PYMKS_USE_FFTW var
    Address #325
    
    Switch USE_FFTW to PYMKS_USE_FFTW so that pyfftw is actually tested.
    
     * make the test fail when using pyfftw to make sure it is actually
       being used on Travis CI.

--- pymks/bases/fftmodule.py
+++ pymks/bases/fftmodule.py
@@ -81,6 +81,7 @@ def import_pyfftw():
     Returns:
       the pyfftw.builders module
     """
+    raise RuntimeError("Using PyFFTW!")
     import pyfftw.builders as pyfftw_fft
     return pyfftw_fft
 
