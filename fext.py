ee704bad7bc01ed0819e702ce702f0abe1c995b3
Daniel Wheeler <daniel.wheeler2@gmail.com>
2017-06-02 16:48:46 -0400

    Functional version of the filter problem

---
+++ fext.py
@@ -0,0 +1,69 @@
+from abc import ABCMeta, abstractmethod
+from typing import TypeVar, Generic, Callable
+
+from toolz.curried import curry, compose, map, pipe
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+class Functor(Generic[A], metaclass=ABCMeta):
+    @abstractmethod
+    def fmap(self, func: Callable[[A], B]) -> 'Functor[B]':
+        return NotImplemented
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+class Maybe(Generic[A], Functor[A], metaclass=ABCMeta):
+    pass
+
+class Just(Generic[A], Maybe[A]):
+    def __init__(self, value: A) -> None:
+        self._value = value
+
+    def fmap(self, func: Callable[[A], B]) -> 'Just[B]':
+        return Just(func(self._value))
+
+    def __str__(self) -> str:
+        return 'Just({value})'.format(value=self._value)
+
+class Nothing(Generic[A], Maybe[A]):
+    def fmap(self, func: Callable[[A], B]) -> Maybe[B]:
+        return Nothing()
+
+    def __str__(self) -> str:
+        return 'Nothing'
+
+class IO(Generic[A], Functor[A]):
+    def __init__(self, action):
+        self._action = action
+
+    def fmap(self, func: Callable[[A], B]) -> 'IO[B]':
+        return IO(lambda: func(self._action()))
+
+    def __call__(self, *args, **kwargs) -> 'IO[A]':
+        return IO(lambda: self._action(*args, **kwargs))
+
+    def __str__(self) -> str:
+        return 'IO({value})'.format(value=self._action())
+
+class Random(Generic[A], Functor[A]):
+    def __init__(self, action):
+        self._action = action
+
+    def fmap(self, func: Callable[[A], B]) -> 'Random[B]':
+        return Random(lambda: func(self._action()))
+
+    def __call__(self, *args, **kwargs) -> 'Random[A]':
+        return Random(lambda: self._action(*args, **kwargs))
+
+    def __str__(self) -> str:
+        return 'Random({value})'.format(value=self._action())
+
+@curry
+def fmap(func: Callable[[A], B], item: Functor[A]) -> Functor[B]:
+    return item.fmap(func)
+
+
+makeio = IO
+makerandom = Random
