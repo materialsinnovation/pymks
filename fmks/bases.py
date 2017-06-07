1824e448f87876bf3ecd69153a89fbf380e67b22
Daniel Wheeler <daniel.wheeler2@gmail.com>
2017-06-07 15:56:10 -0400

    Return redundacy function from primitive basis

--- fmks/bases.py
+++ fmks/bases.py
@@ -5,9 +5,19 @@ from .fext import curry
 def _discretize(x_data, states):
     return 1 - (abs(x_data[..., None] - states)) / (states[1] - states[0])
 
-@curry
-def primitive(x_data, n_states, min_=0., max_=1.):
+def discretize(x_data, n_state, min_=0., max_=1.):
     return np.maximum(
         _discretize(x_data,
-                   np.linspace(min_, max_, n_states)),
+                    np.linspace(min_, max_, n_state)),
         0)
+
+def redundancy(ijk):
+     if np.all(np.array(ijk) == 0):
+         return (slice(None),)
+     else:
+         return (slice(-1),)
+
+@curry
+def primitive_basis(x_data, n_state, min_=0., max_=1.):
+    return {'x_data' : discretize(x_data, n_state, min_=min_, max_=max_),
+            'redundancy_func' : redundancy}
--- fmks/localization.py
+++ fmks/localization.py
@@ -7,54 +7,56 @@ fftn = curry(np.fft.rfftn)
 
 ifftn = curry(np.fft.irfftn)
 
-def s1(ijk):
-    if np.all(np.array(ijk) == 0):
-        return (slice(None),)
-    else:
-        return (slice(-1),)
-
-def s0():
-    return (slice(None),)
-
-def rcond():
-    return np.finfo(float).eps * 1e4
+fftshift = curry(np.fft.fftshift)
 
 @curry
-def lstsq_slice(fx, fy, ijk):
-    return (ijk + s1(ijk),
-            lstsq(fx[s0() + ijk + s1(ijk)],
-                  fy[s0() + ijk],
-                  rcond())[0])
+def lstsq_slice(fx, fy, redundancy_func, ijk):
+    return (ijk + redundancy_func(ijk),
+            lstsq(fx[(slice(None),) + ijk + redundancy_func(ijk)],
+                  fy[(slice(None),) + ijk],
+                  np.finfo(float).eps * 1e4)[0])
 
-def ffit(fx_data, fy_data):
-    lstsq_ijk = lstsq_slice(fx_data, fy_data)
+def fit_fourier(fx_data, fy_data, redundancy_func):
+    lstsq_ijk = lstsq_slice(fx_data, fy_data, redundancy_func)
     return pipe(
         map(lstsq_ijk, np.ndindex(fx_data.shape[1:-1])),
         list,
         array_from_tuple(shape=fx_data.shape[1:], dtype=np.complex)
     )
 
-def faxes(x_data):
-    return np.arange(x_data.ndim - 2) + 1
+def faxes(arr):
+    return np.arange(arr.ndim - 2) + 1
 
 @curry
-def fit(x_data, y_data):
+def fit_disc(x_data, y_data, redundancy_func):
     return pipe(
         [x_data, y_data],
         map(fftn(axes=faxes(x_data))),
-        lambda x: ffit(*x)
+        lambda x: fit_fourier(*x, redundancy_func)
     )
 
 @curry
-def predict(x_data, coeff):
+def fit(x_data, y_data, basis):
+    return fit_disc(y_data=y_data, **basis(x_data))
+
+@curry
+def predict_disc(x_data, coeff):
     return pipe(
-        x_data,
-        fftn(axes=faxes(x_data)),
+        fftn(x_data, axes=faxes(x_data)),
         lambda x: np.sum(x * coeff[None], axis=-1),
         ifftn(axes=faxes(x_data), s=x_data.shape[1:-1])
     ).real
 
+@curry
+def predict(x_data, coeff, basis):
+    return predict_disc(basis(x_data)['x_data'], coeff)
+
+def ini_axes(arr):
+    return np.arange(arr.ndim - 1)
+
 def coeff_to_real(coeff, new_shape):
-    axes = np.arange(coeff.ndim - 1)
-    rcoeff = ifftn(coeff.copy(), axes=axes, s=new_shape)
-    return np.fft.fftshift(rcoeff, axes=axes)
+    return pipe(
+        coeff,
+        ifftn(axes=ini_axes(coeff), s=new_shape),
+        fftshift(axes=ini_axes(coeff))
+    )
