f3615e8524764df11d48f476c4efc534dfc8e47f
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-06-13 14:12:56 -0400

    Implement Nix recipe for fmks
    This recipe only uses a default.nix. There is no need for shell.nix as
    buildPythonPackage automatically uses `python setup.py develop` when
    using shell.nix.

---
+++ default.nix
@@ -0,0 +1,37 @@
+let
+  nixpkgs = import ./nix/nixpkgs_version.nix;
+  pypkgs = nixpkgs.python36Packages;
+  pytest-cov = import ./nix/pytest-cov.nix { inherit nixpkgs; inherit pypkgs; };
+  nbval = import ./nix/nbval.nix { inherit nixpkgs; inherit pypkgs; };
+  scipy = import ./nix/scipy.nix { inherit nixpkgs; inherit pypkgs; };
+  sfepy = import ./nix/sfepy.nix { inherit nixpkgs; inherit pypkgs; };
+  python = pypkgs.python;
+  scikitlearn = pypkgs.scikitlearn.overridePythonAttrs (oldAttrs: {checkPhase=''
+    HOME=$TMPDIR OMP_NUM_THREADS=1 nosetests --doctest-options=+SKIP $out/${python.sitePackages}/sklearn/
+  '';});
+in
+  pypkgs.buildPythonPackage rec {
+    pname = "fmks";
+    version = "0.0.dev66";
+    env = nixpkgs.buildEnv { name=pname; paths=buildInputs; };
+    buildInputs =  [
+      pypkgs.numpy
+      scipy
+      pypkgs.pytest
+      scikitlearn
+      pypkgs.matplotlib
+      pypkgs.sympy
+      pypkgs.cython
+      pypkgs.jupyter
+      pytest-cov
+      nbval
+      nixpkgs.pkgs.git
+      pypkgs.tkinter
+      pypkgs.setuptools
+      sfepy
+      pypkgs.toolz
+      pypkgs.dask
+    ];
+    src=./.;
+    catchConflicts=false;
+  }
