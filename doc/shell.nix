ed512c30c90dced1f2e37c09f5508bb150b4a77c
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-06-18 10:29:25 -0400

    Trying to get build env for sphinx

--- doc/shell.nix
+++ doc/shell.nix
@@ -1,15 +1,16 @@
 let
   nixpkgs = import ../nix/nixpkgs_version.nix;
   pypkgs = nixpkgs.python36Packages;
+  default = import ../default.nix;
+  my_pkgs = default.buildInputs;
 in
   nixpkgs.stdenv.mkDerivation rec {
     name = "pymks-doc-env";
     buildInputs = [
-      pypkgs.python
       pypkgs.sphinx
       pypkgs.virtualenv
       pypkgs.pip
-    ];
+    ] ++ my_pkgs;
     src=null;
     shellHook = ''
     pip install --user sphinx_bootstrap_theme
