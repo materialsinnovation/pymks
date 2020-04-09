ce08d5b616daef02b20354d2bb310662ffe473c8
auag92 <apaar92@gmail.com>
2020-04-09 14:10:57 +0000

    file formatted using black to address travis CI failure

--- pymks/fmks/data/multiphase.py
+++ pymks/fmks/data/multiphase.py
@@ -17,12 +17,7 @@ def _imfilter(x_data, f_data):
     """
     to convolve f_data over x_data
     """
-    return pipe(f_data,
-                ifftshift,
-                fftn,
-                lambda x: conj(x) * fftn(x_data),
-                ifftn,
-                fabs)
+    return pipe(f_data, ifftshift, fftn, lambda x: conj(x) * fftn(x_data), ifftn, fabs)
 
 
 @curry
@@ -46,18 +41,18 @@ def _np_zero_pad(arr, shape):
 
 @curry
 def _gaussian_blur_filter(grain_size, domain_size):
-    return pipe(grain_size,
-                lambda x: fourier_gaussian(np.ones(x), np.ones(len(x))),
-                fftshift,
-                _np_zero_pad(shape=domain_size))
+    return pipe(
+        grain_size,
+        lambda x: fourier_gaussian(np.ones(x), np.ones(len(x))),
+        fftshift,
+        _np_zero_pad(shape=domain_size),
+    )
 
 
 @curry
 def _cumulative_sum(volume_fraction, n_phases):
     if volume_fraction is None:
-        return pipe(n_phases,
-                    lambda x: [1. / x] * x,
-                    lambda x: np.cumsum(x)[:-1])
+        return pipe(n_phases, lambda x: [1.0 / x] * x, lambda x: np.cumsum(x)[:-1])
     if len(volume_fraction) == n_phases:
         if np.sum(volume_fraction) == 1:
             return np.cumsum(volume_fraction)[:-1]
@@ -67,24 +62,20 @@ def _cumulative_sum(volume_fraction, n_phases):
 
 @curry
 def _segmentation_values(x_data, n_samples, volume_fraction, n_phases):
-    return pipe(x_data,
-                lambda x: np.reshape(x, (n_samples, -1)),
-                lambda x: np.quantile(x,
-                                      q=_cumulative_sum(volume_fraction,
-                                                        n_phases),
-                                      axis=1).T,
-                lambda x: np.reshape(x,
-                                     [n_samples, ]
-                                     + [1] * (x_data.ndim - 1)
-                                     + [n_phases - 1, ]))
+    return pipe(
+        x_data,
+        lambda x: np.reshape(x, (n_samples, -1)),
+        lambda x: np.quantile(
+            x, q=_cumulative_sum(volume_fraction, n_phases), axis=1
+        ).T,
+        lambda x: np.reshape(x, [n_samples] + [1] * (x_data.ndim - 1) + [n_phases - 1]),
+    )
 
 
 @curry
-def _npgenerate(n_phases=2,
-                shape=(5, 101, 101),
-                grain_size=(25, 50),
-                volume_fraction=None,
-                seed=10):
+def _npgenerate(
+    n_phases=2, shape=(5, 101, 101), grain_size=(25, 50), volume_fraction=None, seed=10
+):
     """
     Generates a microstructure of dimensions of shape and grains
     with dimensions grain_size.
@@ -99,24 +90,28 @@ def _npgenerate(n_phases=2,
     >>> assert np.allclose(X_gen, X_tru)
     """
     np.random.seed(seed)
-    seg_values = _segmentation_values(n_samples=shape[0],
-                                      volume_fraction=volume_fraction,
-                                      n_phases=n_phases)
+    seg_values = _segmentation_values(
+        n_samples=shape[0], volume_fraction=volume_fraction, n_phases=n_phases
+    )
 
-    return pipe(shape,
-                lambda x: np.random.random(x),
-                _imfilter(f_data=_gaussian_blur_filter(grain_size, shape[1:])),
-                lambda x: x[..., None] >= seg_values(x),
-                lambda x: np.sum(x, axis=-1))
+    return pipe(
+        shape,
+        np.random.random,
+        _imfilter(f_data=_gaussian_blur_filter(grain_size, shape[1:])),
+        lambda x: x[..., None] >= seg_values(x),
+        lambda x: np.sum(x, axis=-1),
+    )
 
 
 @curry
-def generate(n_phases=5,
-             shape=(5, 101, 101),
-             grain_size=(25, 50),
-             volume_fraction=None,
-             seed=10,
-             chunks=()):
+def generate(
+    n_phases=5,
+    shape=(5, 101, 101),
+    grain_size=(25, 50),
+    volume_fraction=None,
+    seed=10,
+    chunks=(),
+):
     """
     Constructs microstructures for an arbitrary number of phases
     given the size of the domain, and relative grain size.
@@ -139,6 +134,7 @@ def generate(n_phases=5,
     ((1,), (3,), (3,))
     >>> assert np.allclose(X_gen.compute(), X_tru)
     """
-    return da.from_array(_npgenerate(n_phases, shape, grain_size,
-                                     volume_fraction, seed),
-                         chunks=(chunks or (-1,)) + shape[1:])
+    return da.from_array(
+        _npgenerate(n_phases, shape, grain_size, volume_fraction, seed),
+        chunks=(chunks or (-1,)) + shape[1:],
+    )
