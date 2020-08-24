fixed nonperiodic correlations and implemented masking

pymks/fmks/correlations.py
@@ -63,7 +63,7 @@ def cross_correlation(arr1, arr2):
         lambda x: daconj(x) * dafftn(arr2, axes=faxes(arr2)),
         daifftn(axes=faxes(arr1)),
         dafftshift(axes=faxes(arr1)),
-        lambda x: x.real / arr1[0].size,
+        lambda x: x.real,
     )
 
 
@@ -144,12 +144,13 @@ def center_slice(x_data, cutoff):
 
 
 @curry
-def two_point_stats(arr1, arr2, periodic_boundary=True, cutoff=None):
+def two_point_stats(arr1, arr2, mask=None, periodic_boundary=True, cutoff=None):
     """Calculate the 2-points stats for two arrays
 
     Args:
       arr1: array used to calculate cross-correlations (n_samples,n_x,n_y)
       arr2: array used to calculate cross-correlations (n_samples,n_x,n_y)
+      mask: array specifying confidence of each pixel (n_samples,n_x,n_y). In range [0,1]. Requires and assumes periodic_boundary to be false.
       periodic_boundary: whether to assume a periodic boundary (default is true)
       cutoff: the subarray of the 2 point stats to keep
 
@@ -163,6 +164,21 @@ def two_point_stats(arr1, arr2, periodic_boundary=True, cutoff=None):
     (2, 5)
 
     """
+    if mask is not None:
+        assert (
+            da.max(mask).compute() <= 1.0 and
+            da.min(mask).compute() >= 0.0
+        ), "Mask must be in range [0,1]!"
+
+        periodic_boundary = False
+        arr1 = arr1 * mask
+        arr2 = arr2 * mask
+
+        mask_array = lambda : mask
+
+    else:
+        mask_array = lambda : np.ones_like(arr1)
+
     cutoff_ = int((np.min(arr1.shape[1:]) - 1) / 2)
     if cutoff is None:
         cutoff = cutoff_
@@ -179,9 +195,14 @@ def two_point_stats(arr1, arr2, periodic_boundary=True, cutoff=None):
 
     padder = identity if periodic_boundary else nonperiodic_padder
 
-    nonperiodic_normalize = lambda x: x / auto_correlation(padder(np.ones_like(arr1)))
+    periodic_normalize = lambda x: x / arr1[0].size
+    # print('Padded Mask Array')
+    # print(nonperiodic_padder(mask_array()).compute())
+    # print('Center Cut Normalization')
+    # print(center_slice(auto_correlation(nonperiodic_padder(mask_array())), cutoff=cutoff).compute())
+    nonperiodic_normalize = lambda x: x / auto_correlation(padder( mask_array() ))
 
-    normalize = identity if periodic_boundary else nonperiodic_normalize
+    normalize = periodic_normalize if periodic_boundary else nonperiodic_normalize
 
     return sequence(
         map_(padder),