cc56209a615425f14021dd7359df33701c3e9545
auag92 <apaar92@gmail.com>
2020-09-17 21:07:28 -0400

    new code added

--- pymks/atommks/atom_stat.py
+++ pymks/atommks/atom_stat.py
@@ -1,44 +1,12 @@
-# author: Apaar Shanker
+"""
+@author: ashanker9@gatech.edu
+"""
 
-
+import ase
 import numba
 import numpy as np
-from scipy.spatial import cKDTree
-from toolz.curried import pipe, curry
-from ase.neighborlist import neighbor_list
+from toolz.curried import pipe, curry, compose
 
-
-fft = curry(np.fft.fft)
-
-ifft = curry(np.fft.ifft)
-
-fftn = curry(np.fft.fftn)
-
-ifftn = curry(np.fft.ifftn)
-
-fftshift = curry(np.fft.fftshift)
-
-ifftshift = curry(np.fft.ifftshift)
-
-conj = curry(np.conj)
-
-func = curry(lambda x, y: conj(x) * fftn(y))
-
-fabs = curry(lambda x: np.absolute(x))
-
-
-@curry
-def imfilter(x_data, f_data):
-    """
-    to convolve f_data over x_data
-    """
-
-    return pipe(f_data,
-                ifftshift,
-                fftn,
-                lambda x: conj(x)*fftn(x_data),
-                ifftn,
-                fabs)
 
 @curry
 def get_scaled_positions(coords, cell, pbc, wrap=True):
@@ -54,7 +22,7 @@ def get_scaled_positions(coords, cell, pbc, wrap=True):
         for i, periodic in enumerate(pbc):
             if periodic:
                 # Yes, we need to do it twice.
-                # See the scaled_positions.py test.
+                # See the scaled_positions.py test in ase library.
                 fractional[:, i] %= 1.0
                 fractional[:, i] %= 1.0
     return fractional
@@ -66,129 +34,62 @@ def get_real_positions(coords, cell):
     return np.dot(cell.T, coords.T).T
 
 
-def sphere(r=10):
-    """
-    args: radius of the sphere
-
-    returns: A 3D cubic matric of dim (2*r+1)^1
-    """
-    return pipe(2*r+1,
-                lambda x: np.mgrid[:x,:x,:x],
-                lambda xx: (xx[0]-r)**2 + (xx[1]-r)**2+(xx[2]-r)**2,
-                lambda x: (x<r*r)*1)
-
-def get_coords(atom):
-    coord_list = []
-    sym_list = np.asarray(sorted(atom.get_chemical_symbols()))
-    syms = np.unique(sym_list)
-
-    coords = atom.get_positions()
-
-    for sym in syms:
-        coord_list.append(coords[np.where(sym_list == sym)[0]])
-    return coord_list
+@curry
+def get_kdTree(coords, cell_dim, cutoff):
+    import MDAnalysis
+    
+    tree = MDAnalysis.lib.pkdtree.PeriodicKDTree(box=cell_dim.astype(np.float32))
+    tree.set_coords(coords.astype(np.float32), 
+                    cutoff=np.float32(cutoff))
+    
+    return tree
 
 
 @curry
-def padder(inp, shape, const_val=0):
-    """
-    args :  input matrix, new shape
+def get_realStats(coords_all, coords_sub, indexes, r_stat, cell, pbc):
+    
+    frac_coords = get_scaled_positions(cell=cell, pbc=pbc, wrap=True)
+    real_coords = get_real_positions(cell=cell)
+    rescale = compose(real_coords, frac_coords)
+    
+    return pipe(indexes, 
+                lambda indxs: rescale(coords_all[indxs[:,1]] - coords_sub[indxs[:,0]] + cell.diagonal()/2), 
+                lambda crds: crds - cell.diagonal()/2)
 
-    returns : matrix reshaped to given shape
-    """
-    ls = np.floor((shape - inp.shape) / 2).astype(int)
-    hs = np.ceil((shape - inp.shape) / 2).astype(int)
-    return np.pad(inp, ((ls[0], hs[0]), (ls[1], hs[1]), (ls[2], hs[2])), 'constant', constant_values=const_val)
+
+@curry
+def get_rdf(coords_stat, r_stat, len_pixel):
+    
+    get_dists = lambda c: np.sqrt(np.sum(c**2, axis=1))
+    
+    nbins = np.round(r_stat*len_pixel).astype(int)+1
+    bins = np.linspace(0.0, r_stat+2, num=nbins)
+    
+    dists = get_dists(coords_stat)
+    rdf, bin_edges = np.histogram(dists, bins)
+
+    bin_centers = (bin_edges[1:] + bin_edges[:-1])/2
+    
+    rdf = rdf / rdf[0]
+    rdf[0] = 0.
+    vols = 4 / 3 * np.pi * (bin_edges[1:]**3 - bin_edges[:-1]**3)
+    pdf = rdf/vols
+    
+    return rdf, pdf, bin_centers
 
 
 @curry
-def compute_rdf(atom, cutoff =5.0, nbins =1001):
-    """
-    returns RDF and bin_edges
-    """
-    N = len(atom)
-
-    bins = np.linspace(0.0, cutoff + 2, nbins)
-
-    i, j, d, D = neighbor_list('ijdD', atom, cutoff=cutoff, self_interaction=False)
-
-    h, bin_edges = np.histogram(d, bins)
-
-    rdf = h / N
-
-    return rdf, bin_edges
-
-@curry
-def compute_rdf_subset(atoms, indexes, cutoff=6.0, nbins=201):
-    """
-    returns RDF computer over a subset of atoms and bin_edges
-    """
-    i, j, d, D = neighbor_list('ijdD', atoms, cutoff=6, self_interaction=False)
-    dlist = []
-    for idx in indexes:
-        dlist.append(d[np.where(i == idx)[0]])
-    dlist = np.concatenate(dlist, axis=0)
-    bins = np.linspace(0.0, cutoff+2, nbins)
-
-    N = len(indexes)
-    h, bin_edges = np.histogram(dlist, bins)
-
-    rdf = h / N
-
-    return rdf, bin_edges
-
-@curry
-def compute_rdf_cross(atom, cutoff_=5.0, nbins_=501, sym1_ = "O", sym2_ = "Si"):
-    """
-    returns auto and cross-RDF for different spcies and bin_edges
-    """
-    syms = np.asarray(atom.get_chemical_symbols())
-    idx_o = np.where(syms == sym1_)[0]
-    idx_s = np.where(syms == sym2_)[0]
-
-    bins = np.linspace(0.0, cutoff_ + 2, 501)
-
-    i, j, d, D = neighbor_list('ijdD', atom, cutoff=cutoff_, self_interaction=False)
-
-    rdf_list = []
-
-    _auxset = set(idx_o)
-    d_list = []
-    for idx in _auxset:
-        a = list(j[np.where(i == idx)[0]])
-        b = list(d[np.where(i == idx)[0]])
-        d_list = d_list + [b[a.index(x)] for x in a if x in _auxset]
-
-    h, bin_edges = np.histogram(d_list, bins)
-
-    rdf = h / len(idx_o)
-    rdf_list.append(rdf)
-
-    _auxset = set(idx_s)
-    d_list = []
-    for idx in _auxset:
-        a = list(j[np.where(i == idx)[0]])
-        b = list(d[np.where(i == idx)[0]])
-        d_list = d_list + [b[a.index(x)] for x in a if x in _auxset]
-
-    h, bin_edges = np.histogram(d_list, bins)
-
-    rdf = h / len(idx_s)
-    rdf_list.append(rdf)
-
-    _auxset = set(idx_s)
-    d_list = []
-    for idx in idx_o:
-        a = list(j[np.where(i == idx)[0]])
-        b = list(d[np.where(i == idx)[0]])
-        d_list = d_list + [b[a.index(x)] for x in a if x in _auxset]
-
-    h, bin_edges = np.histogram(d_list, bins)
-
-    rdf = h / len(idx_o)
-    rdf_list.append(rdf)
-
-    return rdf_list, bin_edges
+def get_2ptStat(coords_stat, r_stat, len_pixel):
+    
+    get_voxel_ids = lambda c, l: (np.round(c * l)).astype(int)
+    
+    coords_indx = get_voxel_ids(coords_stat + r_stat, len_pixel)
+    shape = np.asarray([np.round(r_stat * 2 * len_pixel).astype(int) + 1] * 3)
+    box = box_count(np.zeros(shape), 
+                     coords_indx, 
+                     len(coords_indx), shape)
+  
+    return box / box.max()
 
 
 @numba.njit(parallel=True)
@@ -201,72 +102,3 @@ def box_count(box, indexes, N, shape):
                 if (cz < sz) and (cz >= 0):
                     box[cx, cy, cz] += 1
     return box
-
-
-def get_voxelizedStats(coords1, coords2=None, r_stat=5.0, len_pixel=10, cell=[[1,0,0],[0,1,0],[0,0,1]], pbc=[1,1,1]):
-
-    center = cell.sum(axis=0)*0.5
-
-    if coords2 is None:
-        coords2 = coords1.copy()
-
-    n_atoms1 = coords1.shape[0]
-    n_atoms2 = coords2.shape[0]
-
-    stat_coords = np.zeros([n_atoms1 * n_atoms2, 3])
-
-    for indx, coord in enumerate(coords1):
-        shift = center - coord
-        stat_coords[indx * n_atoms2:(indx+1) * n_atoms2,:] = coords2 + shift
-
-    indexes = pipe(stat_coords,
-                   get_scaled_positions(cell=cell, pbc=pbc),
-                   get_real_positions(cell=cell),
-                   lambda x: x - center + r_stat,
-                   lambda x: (np.round(x * len_pixel)).astype(int))
-
-    shape = [int(r_stat * 2 * len_pixel + 1)] * 3
-
-    box = pipe(shape,
-               lambda shape: np.zeros(shape),
-               lambda box: box_count(box, indexes, indexes.shape[0], shape),
-               lambda box: box / n_atoms1)
-    return box
-
-@curry
-def get_voxelizedStats_tree(coords, coords0, r_stat, len_pixel):
-    tree = cKDTree(coords)
-
-    shape = np.asarray([int(r_stat * 2 * len_pixel + 1)] * 3) # shape of the statistics box
-
-    stat_coords = []
-    count = 0
-    for i, coord in enumerate(coords0[:]):
-        indxs = tree.query_ball_point(coord, 1.8 * r_stat)
-        stat_coords.append(coords[indxs] - coord)
-
-    stat_coords = np.concatenate(stat_coords, axis=0) + r_stat
-
-    indexes = (np.round(stat_coords * len_pixel)).astype(int)
-
-    box = np.zeros(shape)
-    N = indexes.shape[0]
-    box = box_count(box, indexes, N, shape)
-    box = box / len(coords0)
-
-    return box
-
-
-@curry
-def write2vtk(matrix, fname="zeo.vtk"):
-    sx, sy, sz = matrix.shape
-    mx = np.max(matrix)
-    mi = np.min(matrix)
-    lines ='# vtk DataFile Version 2.0\nVolume example\nASCII\nDATASET STRUCTURED_POINTS\nDIMENSIONS %d %d %d\nASPECT_RATIO 1 1 1\nORIGIN 0 0 0\nPOINT_DATA %d\nSCALARS matlab_scalars float 1\nLOOKUP_TABLE default\n'%(sx, sy, sz, matrix.size)
-    with open(fname, 'w') as f:
-        f.write(lines)
-        for ix in range(sz):
-            v = np.ravel(matrix[:,:,ix], order="f")
-            v = ["%1.5f"%x for x in np.round(100 * v / mx)]
-            line = " ".join(v)
-            f.write(line+"\n")
