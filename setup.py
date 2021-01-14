9a827d81a18de673e9c7bf0fd8fb7234658175c3
Daniel Wheeler <daniel.wheeler2@gmail.com>
2021-01-13 19:49:25 -0500

    build: make setup build Graspi correctly
     - ensure build when boost isn't available
     - allow user supplied boost option via env and config
     - don't use try and except for build

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@
 See the documenation for details at https://pymks.org
 """
 
+import configparser
+import pathlib
 import warnings
 import os
 import subprocess
+from distutils.util import strtobool
+
 from setuptools import setup, find_packages, Extension
-import numpy
-from Cython.Build import cythonize
-from pathlib import Path
+from setuptools.config import read_configuration
 
 
 def make_version(package_name):
@@ -76,15 +78,79 @@ def make_version(package_name):
     return version
 
 
-def package_name():
-    return "pymks"
+def get_setupcfg():
+    """Get the absolute path for setup.cfg
+    """
+    return pathlib.Path(__file__).parent.absolute() / "setup.cfg"
+
+
+def get_configuration():
+    """Get contents of setup.cfg as a dict
+    """
+
+    return read_configuration(get_setupcfg())
+
+
+def read_all_config(option):
+    """Read all of the options in setup.cfg not just those used in setup.
+    """
+    parser = configparser.ConfigParser()
+    parser.read(get_setupcfg())
+    if parser.has_option("pymks", option):
+        return parser.getboolean("pymks", option)
+    return False
+
+
+def env_var(var):
+    """Determine the value of an enviroment variable
+
+    Args:
+      var: variable
+
+    Returns:
+      (defined, value): `defined` is bool depending on whether
+        var is defined, `value` is the bool value it's set
+        to (positive if undetermined)
+    """
+    defined = var in os.environ
+    if defined:
+        env_string = os.environ[var]
+        try:
+            value = strtobool(env_string)
+        except ValueError:
+            value = True
+    else:
+        value = False
+    return defined, value
+
+
+def get_name():
+    """Single location for name of package
+    """
+    return get_configuration()["metadata"]["name"]
+
+
+def build_graspi():
+    """Decide whether to build Graspi
+    """
+    env_defined, env_value = env_var("PYMKS_USE_BOOST")
+    if env_defined:
+        return env_value
+    return read_all_config("use-boost")
 
 
 def graspi_path():
-    return "pymks/fmks/graspi"
+    """Find the path to graspi
+    """
+    return list(filter(lambda x: "graspi" in x, find_packages()))[0].replace(".", "/")
 
 
 def graspi_extension():
+    """Configure the graspi extension
+
+    """
+    import numpy  # pylint: disable=import-outside-toplevel
+
     return Extension(
         name=graspi_path().replace("/", ".") + ".graspi",
         sources=[
@@ -94,33 +160,48 @@ def graspi_extension():
         include_dirs=[numpy.get_include(), graspi_path(), "."],
         extra_compile_args=["-std=c++11"],
         language="c++",
+        optional=True,
     )
 
 
-setup(
-    name=package_name(),
-    version=make_version(package_name()),
-    description="Materials Knowledge Systems in Python (PyMKS)",
-    author="Daniel Wheeler",
-    author_email="daniel.wheeler2@gmail.com",
-    url="http://pymks.org",
-    packages=find_packages(),
-    package_data={"": ["tests/*.py"]},
-    install_requires=[
-        "pytest",
-        "numpy",
-        "dask",
-        "Deprecated",
-        "matplotlib",
-        "scikit-learn",
-        "pytest-cov",
-        "nbval",
-        "toolz",
-    ],
-    data_files=["setup.cfg"],
-    ext_modules=cythonize(
-        [graspi_extension()],
-        compiler_directives={"language_level": "3"},
-        include_path=[graspi_path()],
-    ),
-)
+def get_extensions():
+    """Get all extensions, return empty dict if no extension modules
+    activated
+
+    """
+
+    def cythonize(*args, **kwargs):
+        """Only import cython if actually using cython to build.
+        """
+        from Cython.Build import (  # pylint: disable=import-outside-toplevel
+            cythonize as cythonize_,
+        )
+
+        return cythonize_(*args, **kwargs)
+
+    return (
+        dict(
+            ext_modules=cythonize(
+                [graspi_extension()],
+                compiler_directives={"language_level": "3"},
+                include_path=[graspi_path()],
+            )
+        )
+        if build_graspi()
+        else dict()
+    )
+
+
+def setup_args():
+    """Get the setup arguments not configured in setup.cfg
+    """
+    return dict(
+        version=make_version(get_name()),
+        packages=find_packages(),
+        package_data={"": ["tests/*.py"]},
+        data_files=["setup.cfg"],
+        **get_extensions()
+    )
+
+
+setup(**setup_args())
