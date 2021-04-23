2d6340ea9485b75ec161be17bad38ab7d0e84c11
Berkay Yucel <beyucel@gatech.edu>
2021-04-22 23:16:01 -0400

    refactor: make multiphase work with numpy arrays
    Make the multiphase generator work with numpy arrays

--- pymks/fmks/tests/test_multiphase.py
+++ pymks/fmks/tests/test_multiphase.py
@@ -5,7 +5,6 @@
 import numpy as np
 
 import pytest
-import dask.array as da
 
 from pymks.fmks.data.multiphase import generate_multiphase, np_generate
 from pymks.datasets.microstructure_generator import MicrostructureGenerator
@@ -15,9 +14,13 @@ def test_chunking():
     """Test that the generated microstructue is chunked correctly
 
     """
-    da.random.seed(10)
+
     data = generate_multiphase(
-        shape=(5, 11, 11), grain_size=(3, 4), volume_fraction=(0.5, 0.5), chunks=2
+        shape=(5, 11, 11),
+        grain_size=(3, 4),
+        volume_fraction=(0.5, 0.5),
+        chunks=2,
+        seed=10,
     )
     assert data.shape == (5, 11, 11)
     assert data.chunks == ((2, 2, 1), (11,), (11,))
@@ -26,9 +29,8 @@ def test_chunking():
 def test_2d():
     """Regression test for microstructure phases
     """
-    da.random.seed(10)
     data = generate_multiphase(
-        shape=(1, 4, 4), grain_size=(4, 4), volume_fraction=(0.5, 0.5)
+        shape=(1, 4, 4), grain_size=(4, 4), volume_fraction=(0.5, 0.5), seed=10
     )
     assert np.allclose(data, [[[0, 0, 0, 0], [1, 0, 1, 1], [1, 1, 0, 1], [1, 0, 0, 0]]])
 
@@ -36,9 +38,8 @@ def test_2d():
 def test_1d():
     """Test that 1D works
     """
-    da.random.seed(10)
     data = generate_multiphase(
-        shape=(1, 10), grain_size=(4,), volume_fraction=(0.5, 0.5)
+        shape=(1, 10), grain_size=(4,), volume_fraction=(0.5, 0.5), seed=10
     )
     assert np.allclose(data, [0, 0, 0, 0, 1, 0, 1, 1, 1, 1])
 
@@ -48,9 +49,8 @@ def test_grain_size():
     Test incompatible grain_size and shape
     """
     with pytest.raises(RuntimeError) as excinfo:
-        da.random.seed(10)
         generate_multiphase(
-            shape=(1, 10, 10), grain_size=(4,), volume_fraction=(0.5, 0.5)
+            shape=(1, 10, 10), grain_size=(4,), volume_fraction=(0.5, 0.5), seed=10
         )
     assert str(excinfo.value) == "`shape` should be of length `len(grain_size) + 1`"
 
@@ -59,9 +59,8 @@ def test_volume_fraction():
     """Test incoherent volume_fraction
     """
     with pytest.raises(RuntimeError) as excinfo:
-        da.random.seed(10)
         generate_multiphase(
-            shape=(1, 10), grain_size=(2,), volume_fraction=(0.4, 0.4, 0.4)
+            shape=(1, 10), grain_size=(2,), volume_fraction=(0.4, 0.4, 0.4), seed=10
         )
     assert str(excinfo.value) == "The terms in the volume fraction list should sum to 1"
 
@@ -73,12 +72,12 @@ def test_3d():
     different.
 
     """
-    da.random.seed(10)
     data = generate_multiphase(
         shape=(5, 5, 5, 5),
         grain_size=(1, 5, 1),
         volume_fraction=(0.3, 0.3, 0.4),
         chunks=2,
+        seed=10,
     )
     assert data.chunks == ((2, 2, 1), (5,), (5,), (5,))
     assert np.allclose(
