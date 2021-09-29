d4a4b05120f24a0a1d53617c76990289f71e5c1b
auag92 <apaar92@gmail.com>
2021-09-29 16:42:15 +0000

    notebook added

--- pymks/atommks/scripts/canonical_paths.py
+++ pymks/atommks/scripts/canonical_paths.py
@@ -3,121 +3,63 @@ import pickle
 import itertools
 import numpy as np
 from tqdm import tqdm
+from fastdtw import fastdtw
 from multiprocessing import Pool
 from collections import defaultdict
 from itertools import product, combinations
 from toolz.curried import pipe, curry, compose
 from tqdm.contrib.concurrent import process_map
-
-def loader(fname):
-    with open(fname, "rb") as f:
-        return pickle.load(f)
+from sklearn.cluster import AgglomerativeClustering
 
 
-def saver(fname, obj):
-    with open(fname, "wb") as f:
-        pickle.dump(obj, f)
-        
-        
-def dtw_distance(x,y):
-    from fastdtw import fastdtw
-    return fastdtw(x, y, dist=1)[0]
+def calc_path_distance(path1, path2):
+    return fastdtw(path1, path2, dist=1)[0]
+
+
+def calc_func_over_list(func, inputs_list, n_workers):
+    if n_workers == 1:
+        return [func(item) for item in inputs_list]
+    else:
+        return process_map(func, inputs_list, max_workers=n_workers)
 
 
 def calc_path_distances_matrix(paths1, paths2=None, n_workers=1):
     if paths2 is not  None:
-        out = process_map(lambda t: dtw_distance(*t), list(product(paths1, paths2)), max_workers=n_workers)
+        out = calc_func_over_list(func=lambda t: calc_path_distance(*t), 
+                                  inputs_list=list(product(paths1, paths2)), 
+                                  n_workers=n_workers)
         return np.reshape(out, (len(paths1), len(paths2)))
     else:
         l = len(paths1)
-        out = process_map(lambda t: dtw_distance(*t), 
-                          list(combinations(paths1, r=2)), 
-                          max_workers=n_workers)
+        out = calc_func_over_list(func=lambda t: calc_path_distance(*t), 
+                                  inputs_list=list(combinations(paths1, r=2)), 
+                                  n_workers=n_workers)
         mat = np.zeros([l]*2)
         for i0, (ix, iy) in enumerate(combinations(range(l), r=2)):    
             mat[ix, iy] = out[i0]
             mat[iy, ix] = out[i0]
         return mat
 
-    
-def dmatrix(paths1, paths2=None, func=dtw_distance):
-    
-    if paths2 is not  None:
-        mat = np.zeros((len(paths1), len(paths2)))
-        for i1, p1 in enumerate(paths1):
-            for i2, p2 in enumerate(paths2):
-                mat[i1, i2]= func(p1, p2)
-    else:
-        mat = np.zeros([len(paths1)]*2)
-        for i1, p1 in enumerate(paths1):
-            for i2, p2 in enumerate(paths1[i1:]):
-                mat[i1, i1+i2]= func(p1, p2)     
-        out = mat.T + mat
-        mask = np.eye(out.shape[0],dtype=bool)
-        out[mask] = mat[mask]
-        mat = out
-    return mat
-
-
-def get_canonical_paths(paths):
-    
-    from sklearn.cluster import AgglomerativeClustering
-    
-    canonical_paths = []
-    
-    if len(paths) > 1:
-        
-        data_matrix = dmatrix(paths, func=dtw_distance)
-        model = AgglomerativeClustering(affinity='precomputed', 
-                                        n_clusters=None, 
-                                        linkage='complete', 
-                                        distance_threshold=10.).fit(data_matrix)
-        
-        found = set()
-        
-        for i, l in enumerate(model.labels_):
-            if l not in found:
-                found.add(l)
-                canonical_paths.append(paths[i])
-    else:
-        canonical_paths.append(paths[0])
-        
-    return canonical_paths
-
-
-  
-
 
 @curry
 def get_canonical_paths_prll(paths, n_workers=1):
-    
-    from itertools import product
-    from sklearn.cluster import AgglomerativeClustering
-    
     canonical_paths = []
-    
     if len(paths) > 1:
-        
         out = process_map(dtw_distance_tuple, 
                           list(product(paths, paths)), 
                           max_workers=n_workers)
-        
         data_matrix = np.reshape(out, (len(paths), len(paths)))
-        
         model = AgglomerativeClustering(affinity='precomputed', 
                                         n_clusters=None, 
                                         linkage='complete', 
                                         distance_threshold=10.).fit(data_matrix)
-        
         found = set()
-        
         for i, l in enumerate(model.labels_):
             if l not in found:
                 found.add(l)
                 canonical_paths.append(paths[i])
     else:
         canonical_paths.append(paths[0])
-        
     return canonical_paths
 
 
